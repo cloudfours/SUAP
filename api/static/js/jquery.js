cca511d8b073d397c027352f721cda0c2b395d38
angel santibañez <mamorales812@misena.edu.co>
2022-10-13 14:06:08 -0500

    se deja los cambios listos

--- api/static/js/jquery.js
+++ api/static/js/jquery.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
     $('#caso').keyup(function(){
         table.column($(this).data('index')).search(this.value).draw();
     })
+    $('#nombrecompleto').keyup(function(){
+        table.column($(this).data('index')).search(this.value).draw();
+    })
     $('#estado').click(function(){
         table.column($(this).data('index')).search(this.value).draw();
     })
