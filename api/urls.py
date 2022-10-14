17446d186e691dbba03ad1cbf6b9e0f85f239185
angel santibañez <mamorales812@misena.edu.co>
2022-10-14 01:20:33 -0500

    configuracion

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
  path('listMostrarInfo/',perfilUsuario,name='perfil'),
- path('registrarCaso/<int:id>',registrarCaso,name='caso'),
+
  path('editarUsuario/<int:id>',editarUser,name='editarUsuario'),
  path('registrarCaso/',registrarCaso,name='caso'),
  path('editarUsuario/<int:id>',editarUser,name='editarUsuario'),
