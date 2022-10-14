45137bc19d6301bc61bd563069e3eac1a1270ec3
angel santibañez <mamorales812@misena.edu.co>
2022-10-14 17:27:37 -0500

    se sube mejoraas al responsive

--- api/static/services/animaciones.js
+++ api/static/services/animaciones.js
@@ -1,4 +1,33 @@
-$(document).ready(function(){
-    $('#proceso').load().addClase('animacion')
-    console.log('carga')
-})
+// $(document).ready(function () {
+//     var percent = 0;
+
+//     timerId = setInterval(function () {
+//         //increment progress bar
+//         percent += 5;
+//         $('.progress-bar ').css('width', percent + '%');
+//         $('.progress-bar ').attr('aria-valuenow', percent);
+//         $('.progress-bar ').text(percent + '%');
+//        let proceso = $('#proceso').text()
+
+//        console.log(proceso)
+
+
+//         if($('#proceso').html()==='40%'){
+//             if (percent == 40) {
+//               clearInterval(timerId);
+//               $('.proceso').show();
+//           }
+//       }
+//         if($('#abierto').html()==='10%'){
+//           if (percent == 0) {
+//             clearInterval(timerId);
+//             $('.abierto').show();
+//         }
+//     }
+//         else if (percent == 100) {
+//             clearInterval(timerId);
+//             $('.finalizado').show();
+//         }
+
+//     }, 1000);
+// });
